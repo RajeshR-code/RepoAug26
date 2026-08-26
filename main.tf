cae85645039342800eb5ffa14e37150e0fa9905b
Resource Group <rg@121.com>
2026-08-26 15:36:53 +0000

    rr-vnet added

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rgs" {
   name     = "rg-one"
   location = "Central India"
 }
+
+resource "azurerm_network_security_group" "vnet" {
+  name                = "rr-vnet"
+  location            = "Central India"
+  resource_group_name = "rg-one"
+}
