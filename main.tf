7900407c1c32a7f8adf0aa892eaaa2e2eb9e7392
Resource Group <rg@121.com>
2026-08-26 15:40:07 +0000

    subnet added

--- main.tf
+++ main.tf
@@ -8,3 +8,10 @@ resource "azurerm_network_security_group" "vnet" {
   location            = "Central India"
   resource_group_name = "rg-one"
 }
+
+resource "azurerm_subnet" "example" {
+  name                 = "rr-subnet"
+  resource_group_name  = "rr-rg"
+  virtual_network_name = "rr-vnet"
+  address_prefixes     = ["10.0.1.0/24"]
+}
