91ef58c5faaa16836be9fa38353b28f9ceeaec2d
Resource Group <rg@121.com>
2026-08-26 15:54:03 +0000

    nic added

--- main.tf
+++ main.tf
@@ -15,3 +15,15 @@ resource "azurerm_subnet" "subnets" {
   virtual_network_name = "rr-vnet"
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_interface" "nic" {
+  name                = "rr-nic"
+  location            = "central India"
+  resource_group_name = "rg-one"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
